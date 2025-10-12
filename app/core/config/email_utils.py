d45f80b874dca8934984a544722605e606fddb14
Maksym Subchak <donjkee2@gmail.com>
2025-10-12 18:13:23 +0300

    Add user registration route with email activation and error handling

---
+++ app/core/config/email_utils.py
@@ -0,0 +1,30 @@
+import os
+
+from fastapi import Depends
+
+from app.core.notifications.emails import EmailSender
+from app.core.notifications.interfaces import EmailSenderInterface
+
+
+def get_settings() -> BaseAppSettings:
+    environment = os.getenv("ENVIRONMENT", "development")
+    if environment == "testing":
+        return TestingSettings()
+    return Settings
+
+
+def get_accounts_email_notificator(
+    settings: BaseAppSettings = Depends(get_settings)
+) -> EmailSenderInterface:
+    return EmailSender(
+        hostname=settings.EMAIL_HOST,
+        port=settings.EMAIL_PORT,
+        email=settings.EMAIL_HOST_USER,
+        password=settings.EMAIL_HOST_PASSWORD,
+        use_tls=settings.EMAIL_USE_TLS,
+        template_dir=settings.PATH_TO_EMAIL_TEMPLATES_DIR,
+        activation_email_template_name=settings.ACTIVATION_EMAIL_TEMPLATE_NAME,
+        activation_complete_email_template_name=settings.ACTIVATION_COMPLETE_EMAIL_TEMPLATE_NAME,
+        password_email_template_name=settings.PASSWORD_RESET_TEMPLATE_NAME,
+        password_complete_email_template_name=settings.PASSWORD_RESET_COMPLETE_TEMPLATE_NAME
+    )
--- app/routes/accounts.py
+++ app/routes/accounts.py
@@ -1,4 +1,75 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, status, BackgroundTasks, Depends, HTTPException
+from sqlalchemy.exc import SQLAlchemyError
+from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy import select
+
+from app.core.config.email_utils import get_accounts_email_notificator
+from app.core.database import get_db
+from app.core.notifications.interfaces import EmailSenderInterface
+from app.models.accounts import UserModel, UserGroupEnum, ActivationTokenModel, UserGroupModel
+from app.schemas.accounts import UserRegistrationResponseSchema, UserRegistrationRequestSchema
 
 
 router = APIRouter()
+
+
+@router.post(
+    "/register/",
+    response_model=UserRegistrationResponseSchema,
+    summary="User Registration",
+    description="Register a new user with an email and password.",
+    status_code=status.HTTP_201_CREATED,
+)
+async def register_user(
+        user_data: UserRegistrationRequestSchema,
+        background_tasks: BackgroundTasks,
+        db: AsyncSession = Depends(get_db),
+        email_sender: EmailSenderInterface = Depends(get_accounts_email_notificator),
+) -> UserRegistrationResponseSchema:
+    stmt = select(UserModel).where(UserModel.email == user_data.email)
+    result = await db.execute(stmt)
+    existing_user = result.scalars().first()
+    if existing_user:
+        raise HTTPException(
+            status_code=status.HTTP_409_CONFLICT,
+            detail=f"A user with this email {user_data.email} already exists.",
+        )
+
+    stmt = select(UserGroupModel).where(UserGroupModel.name == UserGroupEnum.USER)
+    result = await db.execute(stmt)
+    user_group = result.scalars().first()
+    if not user_group:
+        raise HTTPException(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            detail="Default user group not found."
+        )
+
+    try:
+        new_user = UserModel.create(
+            email=str(user_data.email),
+            raw_password=user_data.password,
+            group_id=user_group.id
+        )
+        db.add(new_user)
+        await db.flush()
+
+        activation_token = ActivationTokenModel(user_id=new_user.id)
+        db.add(activation_token)
+
+        await db.commit()
+        await db.refresh(new_user)
+
+        activation_link = f"http://localhost:8000/api/v1/accounts/activate?token={activation_token.token}"
+        background_tasks.add_task(
+            email_sender.send_activation_email,
+            new_user.email,
+            activation_link,
+        )
+    except SQLAlchemyError as e:
+        await db.rollback()
+        raise HTTPException(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            detail="An error occurred during user creation"
+        ) from e
+    else:
+        return UserRegistrationResponseSchema.model_validate(new_user)
