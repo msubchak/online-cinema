95cf2f8d484ed81ef523783f992f475ccfc2ea7a
Maksym Subchak <donjkee2@gmail.com>
2025-10-18 19:32:13 +0300

    Add endpoint to add a movie to the user's cart

--- app/routes/cart.py
+++ app/routes/cart.py
@@ -1,8 +1,11 @@
 from sqlalchemy import select
 
 from app.core.config.email_utils import get_jwt_auth_manager
-from app.models.cart import CartModel
+from app.models.accounts import UserModel
+from app.models.cart import CartModel, CartItemModel
 from sqlalchemy.ext.asyncio import AsyncSession
+
+from app.models.movies import MovieModel
 from app.schemas.cart import CartResponseSchema, CartMovieSchema
 from fastapi import APIRouter, Depends, HTTPException, status
 from app.core.database import get_db
@@ -47,3 +50,52 @@ async def get_cart_by_user_id(
         user_id=current_user.id,
         movies=movies_list,
     )
+
+
+@router.post(
+    "/",
+    summary="Add a movie to the cart",
+    description="Adds a new movie to the authenticated user's cart",
+    status_code=status.HTTP_201_CREATED,
+)
+async def add_cart_item(
+        movie_id: int,
+        db: AsyncSession = Depends(get_db),
+        current_user: UserModel = Depends(get_current_user),
+):
+    stmt_movie = select(MovieModel).where(MovieModel.id == movie_id)
+    result = await db.execute(stmt_movie)
+    movie = result.scalars().first()
+
+    if not movie:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail="No movie found",
+        )
+
+    stmt_cart = select(CartModel).where(CartModel.user_id == current_user.id)
+    result = await db.execute(stmt_cart)
+    cart = result.scalars().first()
+
+    if not cart:
+        cart = CartModel(user_id=current_user.id)
+        db.add(cart)
+        await db.commit()
+        await db.refresh(cart)
+
+    if any(item.movie_id == movie_id for item in cart.items):
+        raise HTTPException(
+            status_code=status.HTTP_409_CONFLICT,
+            detail="Movie already added to the cart",
+        )
+
+    cart_item = CartItemModel(
+        cart_id=cart.id,
+        movie_id=movie_id,
+    )
+
+    db.add(cart_item)
+    await db.commit()
+    await db.refresh(cart_item)
+
+    return {"detail": "Movie added to the cart successfully"}
