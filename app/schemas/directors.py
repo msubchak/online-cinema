7ae7a818d111d6112c940d0a145ac064cbee0bac
Maksym Subchak <donjkee2@gmail.com>
2025-10-23 18:24:19 +0300

    Add Pydantic schemas for Directors endpoints

---
+++ app/schemas/directors.py
@@ -0,0 +1,36 @@
+from typing import List, Optional
+from pydantic import BaseModel
+
+
+class DirectorsListItemSchema(BaseModel):
+    id: int
+    name: str
+
+
+class DirectorsCreateSchemas(BaseModel):
+    name: str
+
+
+class DirectorsListResponseSchema(BaseModel):
+    directors: List[DirectorsListItemSchema]
+    prev_page: Optional[str]
+    next_page: Optional[str]
+    total_pages: int
+    total_items: int
+
+    model_config = {
+        "from_attributes": True
+    }
+
+
+class DirectorsDetailSchema(BaseModel):
+    id: int
+    name: str
+
+    model_config = {
+        "from_attributes": True
+    }
+
+
+class DirectorsUpdateSchema(BaseModel):
+    name: Optional[str]
