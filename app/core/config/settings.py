8a470257aac203e3f1690e80221b696bb0eaf3e9
Maksym Subchak <donjkee2@gmail.com>
2025-11-01 16:38:13 +0200

    Remove unused PostgreSQL settings

--- app/core/config/settings.py
+++ app/core/config/settings.py
@@ -38,12 +38,6 @@ class BaseAppSettings(BaseSettings):
 
 
 class Settings(BaseAppSettings):
-    POSTGRES_USER: str = os.getenv("POSTGRES_USER", "test_user")
-    POSTGRES_PASSWORD: str = os.getenv("POSTGRES_PASSWORD", "test_password")
-    POSTGRES_HOST: str = os.getenv("POSTGRES_HOST", "test_host")
-    POSTGRES_DB_PORT: int = int(os.getenv("POSTGRES_DB_PORT", 5432))
-    POSTGRES_DB: str = os.getenv("POSTGRES_DB", "test_db")
-
     SECRET_KEY_ACCESS: str = os.getenv(
         "SECRET_KEY_ACCESS",
         base64.urlsafe_b64encode(os.urandom(32)).decode()
